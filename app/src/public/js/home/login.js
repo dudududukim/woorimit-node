9e1e87007c170c08cc9531f6b5f1fd6a7e82c931
dudududukim_home <kdhluck@gmail.com>
2022-05-20 21:58:01 +0900

    using dom user id, pwd object saved

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,3 +1,19 @@
 'use strict';
 
 console.log('hello login');
+
+const id = document.querySelector('#id'),
+    pwd = document.querySelector('#pwd'),
+    loginBtn = document.querySelector('button');
+
+console.log(id);
+
+loginBtn.addEventListener('click', login);
+
+function login(){
+    const req={
+        id: id.value,
+        psword: pwd.value,
+    }
+    console.log(req);
+}
