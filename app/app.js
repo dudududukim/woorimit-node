123901e5125aeede6763aea4c41eda72f911b5e6
dudududukim_home <kdhluck@gmail.com>
2022-05-20 21:15:42 +0900

    folder restruction

--- app/app.js
+++ app/app.js
@@ -4,11 +4,11 @@ const express = require('express');
 const app = express();
 
 //라우팅
-const home = require('./routes/home');
+const home = require('./src/routes/home');
 
 
 //앱 세팅 뭘 보여줄지
-app.set('views', './views');
+app.set('views', './src/views');
 app.set('view engine', 'ejs');
 
 app.use('/', home);
