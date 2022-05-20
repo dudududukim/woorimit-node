8ae8f2dc6880cf3735e9940a95f2310928b77031
dudududukim_home <kdhluck@gmail.com>
2022-05-20 23:15:39 +0900

    login fetch added

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -6,7 +6,7 @@ const id = document.querySelector('#id'),
     pwd = document.querySelector('#pwd'),
     loginBtn = document.querySelector('button');
 
-console.log(id);
+// console.log(id);
 
 loginBtn.addEventListener('click', login);
 
@@ -15,5 +15,15 @@ function login(){
         id: id.value,
         psword: pwd.value,
     }
-    console.log(req);
+    console.log(req, JSON.stringify(req));
+    fetch("/login", {
+        method: "POST",
+        headers:{
+            'Content-Type': "application/json",
+        },
+        body: JSON.stringify(req),
+        // JSON형식으로 데이터 전달할려고 JSON.stringify이용함
+
+    });
+    // fetch는 이 경로에서 데이터를 주고 받겠다는 역할?!
 }
